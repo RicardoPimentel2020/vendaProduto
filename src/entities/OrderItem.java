aee7215ae7150517df0325d462744f213b947ca7
RicardoPimentel2020 <ricardopimentel27@gmail.com>
2020-09-13 17:06:52 -0300

    PROJETO PRODUTO ITEM

---
+++ src/entities/OrderItem.java
@@ -0,0 +1,78 @@
+package entities;
+
+public class OrderItem {
+
+	private Integer quantity;
+	private double price;
+	
+	private Product product;
+	
+	
+	public OrderItem() {
+		
+	}
+
+
+	public OrderItem(Integer quantity, double price, Product produc) {
+	
+		this.quantity = quantity;
+		this.price = price;
+		this.product = produc;
+	}
+
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPriece(double price) {
+		this.price = price;
+	}
+
+
+	public Product getProduc() {
+		return product;
+	}
+
+
+	public void setProduc(Product product) {
+		this.product = product;
+	}
+	
+	public double subTotal() {
+		return price * quantity;
+	
+	}
+	
+	
+	@Override
+	public String toString() {
+		return product.getName() 
+				+ ", $" 
+				+ String.format("%.2f", price) 
+				+ ", Quantity: " 
+				+ quantity + 
+				", Subtotal: $" 
+				+ String.format("%.2f", subTotal());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
